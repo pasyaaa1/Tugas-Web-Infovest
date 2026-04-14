7522cc4069d8075c0c2c8a4db54fe4eb032b0836
EitherKey <gustirizqiputrah@gmail.com>
2026-04-14 16:43:57 +0700

    first commit

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+
+interface GreetingProps {
+  name: string;
+}
+const Greeting: FC<GreetingProps> = ({ name }) => {
+  return <h1>Hello, {name}!</h1>;
+};
+
+export default Greeting;
